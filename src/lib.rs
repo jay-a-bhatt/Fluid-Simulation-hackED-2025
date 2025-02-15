06fc03e2687caec2e2e11a0972e56e7a974829a2
newfish315 <main180360@gmail.com>
2025-02-15 16:56:57 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,45 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
-//
-const WASM_MEMORY_BUFFER_SIZE: usize = 4;
-static mut WASM_MEMORY_BUFFER: [f32; WASM_MEMORY_BUFFER_SIZE] = [0.0; WASM_MEMORY_BUFFER_SIZE];
+const WASM_MEMORY_BUFFER_SIZE: usize = 8;
+static mut WASM_MEMORY_BUFFER: [f32; WASM_MEMORY_BUFFER_SIZE] = [0.0; INSTANCE_DATA_SIZE]; 
 
+/* Initialize the buffer with values manually
 #[wasm_bindgen]
-pub fn init_buffer() {
-    unsafe {
-        // Initialize the buffer with values
-        WASM_MEMORY_BUFFER[0] = 1.1;
-        WASM_MEMORY_BUFFER[1] = 2.2;
-        WASM_MEMORY_BUFFER[2] = 3.3;
-        WASM_MEMORY_BUFFER[3] = 4.4;
-    }
-}
-
-#[wasm_bindgen]
-pub fn buffer_pointer() -> *const f32 {
-    let p: *const f32;
-    unsafe {
-        p = WASM_MEMORY_BUFFER.as_ptr();
-    }
-    return p;
-}
-
-#[link(wasm_import_module = "index.js")] //importing js functions
-extern
+pub fn init_buffer()
 {
-    fn context() -> (i32, i32);
+    unsafe
+    {
+        // WASM_MEMORY_BUFFER[0] = 1.1;
+    }
+}
+*/
+
+const INSTANCE_DATA_SIZE: usize = 4+2+2;
+const max_instances: u32 = 100;
+static mut current_instance: usize = 0; // increments 0 - 799 = 800x before being reset
+
+pub unsafe fn draw_circle(r: f32, g: f32, b: f32, a: f32, x: f32, y: f32, s_x: f32, s_y: f32)
+{
+    let mut array: [f32; 8] = [r, g, b, a, x, y, s_x, s_y];
+    let instance_offset: usize = current_instance * 8; // instance offset corresponds to total # of floats in the buffer
+    for i in 0.. 8
+    {
+        let index = instance_offset + i;
+        WASM_MEMORY_BUFFER[index] = array[i];
+    }
+    if current_instance == 100
+    {
+        current_instance = 0;
+    }
+}
+
+#[wasm_bindgen]
+pub fn update()
+{
+    //
 }
 
 fn main()
 {
-    let width;
-    let height;
-    (width, height) = index.js.context();
-}
-
-#[wasm_bindgen]
-pub fn update() -> i32;
-{
-    //
-    return 1;
+    unsafe { draw_circle(50.5, 100.1, 120.5, 200.0, 123.2, 54.3, 40.12, 43.54);}
 }
