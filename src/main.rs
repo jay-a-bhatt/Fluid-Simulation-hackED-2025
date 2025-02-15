28e012bdc63396b08acb2519dd944c97978c0f3f
Jay Bhatt <bbbaj2404@gmail.com>
2025-02-15 11:51:03 -0700

    Snake case + Impl

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,55 @@
 struct FlipFluid {
     density: i32,
-    fNumX: f32,
-    fNumY: f32,
+    f_num_x: f32,
+    f_num_y: f32,
     h: i32,
-    fInvSpacing: f32,
-    fNumCells: i32,
+    f_inv_spacing: f32,
+    f_num_cells: i32,
 
     u: Vec<f32>,
     v: Vec<f32>,
     du: Vec<f32>,
     dv: Vec<f32>,
-    prevU: Vec<f32>,
-    prevV: Vec<f32>,
+    prev_u: Vec<f32>,
+    prev_v: Vec<f32>,
     p: Vec<f32>,
     s: Vec<f32>,
-    cellType: Vec<f32>,
-    cellColour: Vec<f32>,
+    cell_type: Vec<f32>,
+    cell_colour: Vec<f32>,
 
-    maxParticles: i32,
+    max_particles: i32,
 
-    particlePos: Vec<f32>,
-    particleColour: Vec<f32>,
+    particle_pos: Vec<f32>,
+    particle_colour: Vec<f32>,
 
-    particleVel: Vec<f32>,
-    particleDensity: Vec<f32>,
-    particleRestDensity: Vec<f32>,
+    particle_vel: Vec<f32>,
+    particle_density: Vec<f32>,
+    particle_rest_density: Vec<f32>,
 
-    particleRadius: i32,
-    pInvSpacing: f32,
-    pNumX: f32,
-    pNumY: f32,
-    pNumCells: f32,
+    particle_radius: i32,
+    p_inv_spacing: f32,
+    p_num_x: f32,
+    p_num_y: f32,
+    p_num_cells: f32,
 
-    numCellParticles: Vec<i32>,
-    firstCellParticles: Vec<i32>,
-    cellParticleIds: Vec<i32>,
+    num_cell_particles: Vec<i32>,
+    first_cell_particles: Vec<i32>,
+    cell_particle_ids: Vec<i32>,
 
-    numParticles: i32,
+    num_particles: i32,
 }
 
-impl FlipFluid {}
+impl FlipFluid {
+    fn new(
+        density: i32,
+        width: i32,
+        height: i32,
+        spacing: i32,
+        particle_radius: i32,
+        max_particles: i32,
+    ) {
+    }
+}
 
 fn main() {
     println!("Hello, world!");
