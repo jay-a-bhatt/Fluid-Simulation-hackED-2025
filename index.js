5e994d525b049405a94e9a1d8efdaa5173486d92
newfish315 <main180360@gmail.com>
2025-02-15 11:46:44 -0700

    cargo toml + index

---
+++ index.js
@@ -0,0 +1,8 @@
+import wasmInit from "./pkg/Fluid_Simulation_hackED_2025.js";
+
+const runWasm = async () => {
+  // instantiate wasm module
+  const rustWasm = await wasmInit("./pkg/Fluid_Simulation_hackED_2025_bg.wasm");
+
+};
+runWasm();
