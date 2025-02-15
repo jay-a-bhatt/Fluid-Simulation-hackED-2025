f4f07ed362d0ad667d216aae43a1aeb89c9a5e0d
McConnellNA <RylanJMcConnell@gmail.com>
2025-02-15 11:49:35 -0700

    Create integrate_particles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
 }
+fn integrate_particles(dt: f32 , gravity: f32){
+    let num_particles = 0;
+    for i in 0..num_particles{
+
+    }
+    //Replace arrays with particles
+    todo!()
+}
