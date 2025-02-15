6d4bfe1a4346fb479366e319a55c972cb84c2524
Rordon <65141265+RordyC@users.noreply.github.com>
2025-02-15 11:30:30 -0700

    WebGPU initialization

---
+++ webgpu.js
@@ -0,0 +1,57 @@
+console.log("Hello from WebGPU js file! :)");
+const rand = (min, max) => {
+
+    if (min === undefined)
+    {
+        min = 0;
+        max = 1;
+    }
+    else if (max === undefined)
+    {
+        max = min;
+        min = 0;
+    }
+
+    return min + Math.random() * (max - min);
+};
+
+async function initWebGPU()
+{
+    if (!navigator.gpu)
+    {
+        console.error('This browser does not support WebGPU');
+        return;
+    }
+    const adapter = await navigator.gpu?.requestAdapter();
+    if (!adapter)
+    {
+        console.error('No WebGPU adapters found');
+        return;
+    }
+
+    let device = await adapter.requestDevice();
+    device.lost.then((info) =>
+    {
+        console.error(`WebGPU device was lost: ${info.message}`)
+        device = null;
+        if (info.reason != 'destroyed')
+        {
+            initWebGPU();
+        }
+    });
+
+    console.log("WebGPU Initialized Successfully");
+    const circleShaderSrc = await fetch("./shaders/circle.wgsl").then(x => x.text())
+
+    main(device, circleShaderSrc);
+}
+
+function main(device, circleShaderSrc)
+{
+    const canvas = document.querySelector('canvas');
+    const presentationFmt = navigator.gpu.getPreferredCanvasFormat();
+    const context = canvas.getContext('webgpu');
+    context.configure({device, format: presentationFmt} )
+}
+
+initWebGPU();
