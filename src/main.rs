92774dc1b1282d49228f3153098277d24b77afdb
McConnellNA <RylanJMcConnell@gmail.com>
2025-02-15 17:15:53 -0700

    Complete Transfer_vel

--- src/main.rs
+++ src/main.rs
@@ -356,7 +356,7 @@ impl FlipFluid {
     }
 
     fn transfer_velocities(mut self, to_grid: bool, flip_ratio: f32) {
-        let n: i32 = self.f_num_y;
+        let n: f32 = self.f_num_y as f32;
         let h: f32 = self.h;
         let h1: f32 = self.f_inv_spacing;
         let h2: f32 = h * 0.5;
@@ -383,7 +383,7 @@ impl FlipFluid {
                 let y: f32 = self.particle_pos[(2 * i + 1) as usize];
                 let xi: i32 = clamp((x * h1).floor(), 0.0, (self.f_num_x - 1) as f32);
                 let yi: i32 = clamp((y * h1).floor(), 0.0, (self.f_num_y - 1) as f32);
-                let cell_nr: i32 = xi * n + yi;
+                let cell_nr: i32 = xi * n as i32+ yi;
 
                 if self.cell_type[cell_nr as usize] == var::AIR_CELL {
                     self.cell_type[cell_nr as usize] = var::FLUID_CELL;
@@ -392,10 +392,117 @@ impl FlipFluid {
         }
 
         for component in 0..2 {
+            let mut dx: f32;
+            let mut dy: f32;
+
+            let mut f: Vec<f32>;
+            let mut prev_f: Vec<f32>;
+            let mut d: Vec<f32>; 
+
             if component == 0 {
-                let dx: f32 = 0.0;
+                dx = 0.0;
+                dy = h2;
+
+                f = self.u;
+                prev_f = self.prev_u;
+                d = self.du;
             } else {
-                let dx: f32 = h2;
+                dx = h2;
+                dy = 0.0;
+
+                f = self.v;
+                prev_f = self.prev_v;
+                d = self.dv;
+            }
+
+            for i in 0..self.num_particles{
+                let mut x: f32 = self.particle_pos[(2*i) as usize];
+                let mut y: f32 = self.particle_pos[(2*i+1) as usize];
+
+                x = clamp(x,h,(self.f_num_x as f32 - 1.0) * h) as f32;
+                y = clamp(y,h,(self.f_num_y as f32 - 1.0) * h) as f32;
+
+                let x0: f32 = f32::min(((x-dx)*h1).floor(),self.f_num_x as f32-2.0);
+                let tx: f32 = ((x-dx)- x0 * h) * h1;
+                let x1: f32 = f32::min((x0+1.0), self.f_num_x as f32 -2.0);
+
+                let y0: f32 = f32::min(((y-dy)*h1).floor(), self.f_num_y as f32-2.0);
+                let ty: f32 = ((y - dy)- y0*h)*h1;
+                let y1: f32 = f32::min(y0+1.0, self.f_num_y as f32 - 2.0);
+
+                let sx: f32 = 1.0 - tx;
+                let sy: f32 = 1.0 - ty;
+
+                let d0: f32 = sx*sy;
+                let d1: f32 = tx*sy;
+                let d2: f32 = tx*ty;
+                let d3: f32 = sx*ty;
+
+                let nr0: f32 = x0*n+y0;
+                let nr1: f32 = x1*n+y0;
+                let nr2: f32 = x1*n+y1;
+                let nr3: f32 = x0*n+y1;
+
+                if to_grid{
+                    let pv: f32 = self.particle_vel[(2*i+component) as usize];
+                    f[nr0 as usize] += pv * d0; d[nr0 as usize] += d0;
+                    f[nr1 as usize] += pv * d1; d[nr1 as usize] += d1;
+                    f[nr2 as usize] += pv * d2; d[nr2 as usize] += d2;
+                    f[nr3 as usize] += pv * d3; d[nr3 as usize] += d3;
+                }else{
+                    let mut offset: f32 = 1.0;
+                    let mut valid0: f32 = 0.0;
+                    let mut valid1: f32 = 0.0;
+                    let mut valid2: f32 = 0.0;
+                    let mut valid3: f32 = 0.0;
+                    if component == 0{
+                        offset = n;
+                    }
+                    if (self.cell_type[nr0 as usize] != var::AIR_CELL || self.cell_type[nr0 as usize - offset as usize] != var::AIR_CELL){
+                        valid0 = 1.0;
+                    }
+                    if (self.cell_type[nr1 as usize] != var::AIR_CELL || self.cell_type[nr1 as usize - offset as usize] != var::AIR_CELL){
+                        valid1 = 1.0;
+                    }
+                    if (self.cell_type[nr2 as usize] != var::AIR_CELL || self.cell_type[nr2 as usize - offset as usize] != var::AIR_CELL){
+                        valid2 = 1.0;
+                    }
+                    if (self.cell_type[nr3 as usize] != var::AIR_CELL || self.cell_type[nr3 as usize - offset as usize] != var::AIR_CELL){
+                        valid3 = 1.0;
+                    }
+
+                    let v: f32 = self.particle_vel[(2*i+component) as usize];
+                    let d: f32 = valid0 * d0 + valid1 *d1 + valid2 * d2 + valid3 * d3;
+
+                    if d > 0.0 {
+                        let pic_v: f32 = (valid0*d0*f[nr0 as usize]+valid1*d1*f[nr1 as usize]+valid2*d2*f[nr2 as usize]+valid3*d3*f[nr3 as usize])/d;
+                        let corr: f32 = (valid0*d0*(f[nr0 as usize])-prev_f[nr0 as usize]+valid1*d1*(f[nr1 as usize])-prev_f[nr1 as usize]+valid2*d2*(f[nr2 as usize])-prev_f[nr2 as usize]+valid3*d3*(f[nr3 as usize])-prev_f[nr3 as usize])/d;
+                        let flip_v: f32 = v + corr;
+
+                        self.particle_vel[(2*i+component) as usize] = (1.0 - flip_ratio) * pic_v + flip_ratio * flip_v;
+                    }
+                }
+            }
+            if to_grid {
+                for i in 0..f.len(){
+                    if d[i as usize] > 0.0{
+                        f[i as usize] /= d[i as usize];
+                    }
+                }
+            
+                // restore solid cells
+            
+                for i in 0..self.f_num_x{
+                    for j in 0..self.f_num_y{
+                        let solid = self.cell_type[(i as f32 * n + j as f32) as usize] == var::SOLID_CELL;
+                        if solid || (i > 0 && self.cell_type[((i - 1) as f32 * n + j as f32) as usize] == var::SOLID_CELL){
+                            self.u[(i as f32 * n + j as f32) as usize] = self.prev_u[(i as f32 * n + j as f32) as usize];
+                        }
+                        if solid || (j > 0 && self.cell_type[(i as f32 * n + j as f32 - 1.0) as usize] == var::SOLID_CELL){
+                            self.v[(i as f32 * n + j as f32) as usize] = self.prev_v[(i as f32 * n + j as f32) as usize];
+                        }
+                    }
+                }
             }
         }
     }
